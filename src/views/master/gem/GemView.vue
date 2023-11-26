62e1a94e3c7349c328aa46a97548deb73b48d34c
coqo <chaisurs@scg.com>
2023-11-26 14:21:07 +0700

    upd

--- src/views/master/gem/GemView.vue
+++ src/views/master/gem/GemView.vue
@@ -78,8 +78,15 @@
               <i class="bi bi-gem"></i>
             </div> -->
             <!-- <pdf class="btn btn-sm btn-info" :modelValue="slotProps.data"></pdf> -->
-            <button class="btn btn-sm btn btn-main" @click="onDelete(slotProps.data)">
+            <!-- <button class="btn btn-sm btn btn-main" @click="onDelete(slotProps.data)">
               <i class="bi bi-trash-fill"></i>
+            </button> -->
+            <button
+              title="เเก้ไข"
+              class="btn btn-sm btn btn-main"
+              @click="onUpdate(slotProps.data)"
+            >
+              <i class="bi bi-pencil-fill"></i>
             </button>
           </div>
         </template>
@@ -110,6 +117,12 @@
       @closeModal="closeModalAddGem"
       @fetch="onSearchByAdd"
     ></modalAddGem>
+    <modalUpdGem
+      :isShowModal="isShowModalUpdGem"
+      :modelMaster="dataUpd"
+      @closeModal="closeModalAddGem"
+      @fetch="onSearchByAdd"
+    ></modalUpdGem>
   </div>
 </template>
 
@@ -124,6 +137,7 @@ import { formatDate, formatDateTime } from '@/utils/moment'
 import swAlert from '@/js/alert/sweetAlerts.js'
 
 import modalAddGem from './components/ModalFormCreate.vue'
+import modalUpdGem from './components/ModalFromUpdate.vue'
 
 const pageTitle = defineAsyncComponent(() => import('@/components/custom/PageTitle.vue'))
 const loading = defineAsyncComponent(() => import('@/components/overlay/loading-overlay.vue'))
@@ -133,17 +147,20 @@ export default {
     DataTable,
     Column,
     loading,
-    modalAddGem
+    modalAddGem,
+    modalUpdGem
   },
   data() {
     return {
       isLoading: false,
       isShowModalAddGem: false,
+      isShowModalUpdGem: false,
       // table
       totalRecords: 0,
       take: 10, //all
       skip: 0,
       data: [],
+      dataUpd: {},
 
       //search
       search: {
@@ -153,7 +170,7 @@ export default {
   },
   methods: {
     handlePageChange(e) {
-      console.log('page change')
+      //console.log('page change')
       console.log(e)
     },
 
@@ -206,7 +223,13 @@ export default {
     },
     onSearchByAdd() {
       this.isShowModalAddGem = false
+      this.isShowModalUpdGem = false
+      this.dataUpd = {}
       this.fetchMasterGem()
+    },
+    onUpdate(e) {
+      this.dataUpd = { ...e }
+      this.isShowModalUpdGem = true
     },
     // fetchData() {
     //   this.fetchMasterGem()
@@ -244,6 +267,8 @@ export default {
     },
     closeModalAddGem() {
       this.isShowModalAddGem = false
+      this.isShowModalUpdGem = false
+      this.dataUpd = {}
     },
 
     // ------ helper ------//
